2ceec783bc9cb276b75ae0db3a76ea1789adeac0
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-02-10 13:36:14 -0500

    Description column added with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_10_182639) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_10_183444) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
 end
