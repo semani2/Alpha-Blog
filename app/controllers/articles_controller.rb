0605733c2e45d2b755a544c567976044ae2fe5df
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-03-01 21:07:30 -0500

    Creating new article from browser completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,20 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+
+  end
+
+  def create
+    #render plain: params[:article]
+    # Creating a new article from the params received on the post call.
+    # Additionally, we are white listing title and description
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+
+    # get this article_path from rails routes --expanded
+    # can also use redirect_to @article
+    redirect_to article_path(@article)
+  end
 end
