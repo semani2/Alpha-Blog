4d8e39c26f51890a6956c4eefe38086af0680919
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-03-02 10:24:56 -0500

    Adding links to help navigation in the app

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # About page
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles
 end
