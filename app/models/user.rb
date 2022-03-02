f5450624bee137d12226ba9a0d35dd84f3d9447d
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-03-02 15:14:07 -0500

    Converting email to lowercase before saving

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
 
   has_many :articles
 
+  before_save { self.email = email.downcase}
+
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
@@ -11,4 +13,5 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }
+
 end
