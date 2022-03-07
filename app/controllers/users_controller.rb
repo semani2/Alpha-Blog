e07c111f9d90de283ac415d1d2ff32ecd42d2c2f
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-03-07 13:45:15 -0500

    Helper methods added, signup sets session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to Alpha Blog, #{@user.username}, you have successfully signed up"
       redirect_to articles_path
     else
