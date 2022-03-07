991ba3df048a3e1fddfc13c2f20f4c37417a1b16
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-03-07 13:35:40 -0500

    Login and logout implemented

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,23 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(email: params[:session][:email].downcase)
+    # Checking if user is found
+    if user && user.authenticate(params[:session][:password])
+      # Rails provided session object
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      # .now has validity for one http request
+      flash.now[:alert] = "There was something wrong with your login details!"
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:notice] = "Logged out successfully!"
+    redirect_to root_path
   end
 end
