2e6dadd131fd3e328bef9471988949443a8cc490
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-03-07 13:21:24 -0500

    Login route, session controller setup done

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   # Signup page
   get 'signup', to: 'users#new'
 
+  # Login page
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   resources :articles
 
