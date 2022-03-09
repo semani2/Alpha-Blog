9fe07f29039ea76f31e9e42a7374801099ae71ac
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-03-09 15:15:38 -0500

    Admin functionality added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:alert] = "You can only edit your own profile"
       redirect_to @user
     end
