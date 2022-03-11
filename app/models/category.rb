b7f4e000e1e6a3df1b3c30b862acdb32fe1965f3
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-03-10 20:58:13 -0500

    Validations added through TDD

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
 
+  # Validates that no article is created with an empty title with a min and max length
+  validates :name, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 2, maximum: 25 }
+
 end
