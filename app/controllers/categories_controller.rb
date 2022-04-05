4967813bad60ae9fafffb2a577a4d10ae9db7b26
Sai Prajnan Emani <saiprajnanemani@gmail.com>
2022-04-04 21:22:25 -0400

    Creating new category implemented with tests

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,19 @@
 class CategoriesController < ApplicationController
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:notice] = "Category was successfully created"
+      redirect_to @category
+    else
+      render 'new'
+    end
+  end
+
   def show
 
   end
@@ -8,7 +22,10 @@ class CategoriesController < ApplicationController
 
   end
 
-  def new
+  private
 
+  def category_params
+    params.require(:category).permit(:name)
   end
+
 end
